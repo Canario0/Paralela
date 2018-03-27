190a53b1d6942543ef9463b2c4cdc09007122b46
Canario0 <prenero4bc@gmail.com>
2018-03-27 12:23:47 +0200

    Aplicadas las fórmulas

--- energy_mpiseq/energy_mpi.c
+++ energy_mpiseq/energy_mpi.c
@@ -24,7 +24,6 @@ typedef struct
 	int *posval;
 } Storm;
 
-
 /* FUNCIONES AUXILIARES: No se utilizan dentro de la medida de tiempo, dejar como estan */
 /* Funcion de DEBUG: Imprimir el estado de la capa */
 void debug_print(int layer_size, float *layer, int *posiciones, float *maximos, int num_storms)
@@ -149,17 +148,25 @@ int main(int argc, char *argv[])
 	double ttotal = cp_Wtime();
 
 	/* COMIENZO: No optimizar/paralelizar el main por encima de este punto */
-	float *raiz;
 	float *layer = (float *)malloc(sizeof(float) * layer_size);
-	int layersize = layer_size / size;
-	float *miniLayer = (float *)malloc(sizeof(float) * layersize);
+
+	int local_layer_size = layer_size / size;
+	if(rank< layer_size%size) local_layer_size+=1;
+
+	int desplazamiento = (rank > layer_size % size) ? rank * (local_layer_size) + layer_size % size : rank * (local_layer_size) + rank;
+	// if(rank > layer_size%size)
+	// 	desplazamiento += layer_size%size;
+	// else
+	// 	desplazamiento+=rank;
+
+	float *miniLayer = (float *)malloc(sizeof(float) * local_layer_size);
 	if (layer == NULL)
 	{
 		fprintf(stderr, "Error: Allocating the layer memory\n");
 		exit(EXIT_FAILURE);
 	}
 	/* 3. Reservar memoria para las capas e inicializar a cero */
-	float *layer_copy = (float *)malloc(sizeof(float) * layersize);
+	float *layer_copy = (float *)malloc(sizeof(float) * local_layer_size);
 
 	if (miniLayer == NULL || layer_copy == NULL)
 	{
@@ -167,16 +174,15 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	for (k = 0; k < layersize; k++)
+	for (k = 0; k < local_layer_size; k++)
 		miniLayer[k] = 0.0f;
-	for (k = 0; k < layersize; k++)
+	for (k = 0; k < local_layer_size; k++)
 		layer_copy[k] = 0.0f;
 
-	raiz = (float *)malloc(sizeof(float) * layer_size);
+	float *raiz = (float *)malloc(sizeof(float) * layer_size);
 
 	for (int i = 0; i < layer_size; i++)
 		raiz[i] = sqrtf(i + 1);
-	// raiz[i] = sqrtf(i + 1 + rank * (layersize));
 
 	/* 4. Fase de bombardeos */
 	for (i = 0; i < num_storms; i++)
@@ -192,7 +198,7 @@ int main(int argc, char *argv[])
 			int posicion = storms[i].posval[j * 2];
 
 			/* Para cada posicion de la capa */
-			for (k = 0; k < layersize; k++)
+			for (k = 0; k < local_layer_size; k++)
 			{
 				/* Actualizar posicion */
 
@@ -209,9 +215,9 @@ int main(int argc, char *argv[])
 				//float atenuacion = sqrtf((float)distancia);
 
 				/* 4. Calcular energia atenuada */
-				//printf("Posicion: %d", posicion - (k + rank * (layersize)));
+				//printf("Posicion: %d", posicion - (k + rank * (local_layer_size)));
 				//fflush(stdout);
-				float energia_k = energia / raiz[abs(posicion - (k + rank * (layersize)))];
+				float energia_k = energia / raiz[abs(posicion - (k + desplazamiento))];
 
 				/* 5. No sumar si el valor absoluto es menor que umbral */
 				if (energia_k >= UMBRAL)
@@ -221,7 +227,7 @@ int main(int argc, char *argv[])
 
 		/* 4.2. Relajacion entre tormentas de particulas */
 		/* 4.2.1. Copiar valores a capa auxiliar */
-		for (k = 0; k < layersize; k++)
+		for (k = 0; k < local_layer_size; k++)
 			layer_copy[k] = miniLayer[k];
 
 		/* 4.2.2. Actualizar capa, menos los extremos, usando valores del array auxiliar */
@@ -234,8 +240,8 @@ int main(int argc, char *argv[])
 		if (rank != size - 1)
 		{
 			MPI_Recv(&fin, 1, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD, &status);
-			miniLayer[layersize - 1] = (layer_copy[layersize - 1 - 1] + layer_copy[layersize - 1] + fin) / 3;
-			MPI_Send(&layer_copy[layersize - 1], 1, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD);
+			miniLayer[local_layer_size - 1] = (layer_copy[local_layer_size - 1 - 1] + layer_copy[local_layer_size - 1] + fin) / 3;
+			MPI_Send(&layer_copy[local_layer_size - 1], 1, MPI_FLOAT, rank + 1, 0, MPI_COMM_WORLD);
 		}
 		if (rank != 0)
 		{
@@ -243,16 +249,16 @@ int main(int argc, char *argv[])
 			miniLayer[0] = (ini + layer_copy[0] + layer_copy[0 + 1]) / 3;
 		}
 
-		for (k = 1; k < layersize - 1; k++)
+		for (k = 1; k < local_layer_size - 1; k++)
 			miniLayer[k] = (layer_copy[k - 1] + layer_copy[k] + layer_copy[k + 1]) / 3;
 
-		MPI_Gather(miniLayer, layersize, MPI_FLOAT, layer, layersize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		MPI_Gather(miniLayer, local_layer_size, MPI_FLOAT, layer, local_layer_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
 		// for (int i = 0; i < size; i++)
 		// {
 		// 	if (i == rank)
 		// 	{
 		// 		printf("[%i] ", rank);
-		// 		for (int j = 0; j < layersize; j++)
+		// 		for (int j = 0; j < local_layer_size; j++)
 		// 			printf("%f ", miniLayer[j]);
 		// 		printf("\n");
 		// 	}
