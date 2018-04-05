a60ff3eb8643660e2bcfa7b239b67bca15c22ed3
Canario0 <prenero4bc@gmail.com>
2018-04-05 11:19:55 +0200

    Limpiando el repo

--- energy_mpiseq/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mpirun -n $1 ./energy 20 polla1; mpirun -n 1 ./secuencial 20 polla1
-mpirun -n $1 ./energy 20 polla1  polla2; mpirun -n 1 ./secuencial 20 polla1 polla2
-mpirun -n $1 ./energy 20 polla1  polla2 polla3; mpirun -n 1 ./secuencial 20 polla1 polla2 polla3
